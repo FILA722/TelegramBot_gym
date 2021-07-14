2228eb854b384963226c9ff00a821d99cda6dffc
Alex Fila <filal41sng@gmail.com>
2021-07-14 15:22:44 +0300

    optimized function "registry" renamed some variables

--- gym_registry_bot_m3.py
+++ gym_registry_bot_m3.py
@@ -8,7 +8,7 @@ import gym_registry_bot_alarm
 import confidential
 
 bot = telebot.TeleBot(confidential.bot_id)
-file_dir = confidential.file_dir
+registry_file_dir = confidential.file_dir #директория, где будут храниться файлы со списком клиентов, которые записались на занятие
 spy_file = confidential.spy_file
 
 REG_VALUE = 3 #кол-во записей, котрые можно сделать с одного id в день.
@@ -28,9 +28,12 @@ month = {
         '12':'грудня'
         }
 #список с данными от пользователя. (сегодня/завтра, время записи, user_id)
-data = []
+client_registry_data = []
 
 def spy(user_id):
+    """
+    записывает в директорию spy_file файл с записью user_id : колличество запросов
+    """
     with open(spy_file, 'r') as spy_f:
         dict_id = json.load(spy_f)
         if user_id in dict_id.keys():
@@ -43,7 +46,8 @@ def spy(user_id):
 
 def check_registry(name, time_):
     """
-    тут проверяеться валидность имени и наличие записи
+    тут проверяеться валидность имени и имееться ли запись за этим именем
+    на выходе вызывает ф-цию регистрации записи.
     """
     input_name = name.split(' ', 1)
     second_name_sep, name_sep = input_name[0], input_name[1]
@@ -51,14 +55,14 @@ def check_registry(name, time_):
     second_name_sep = second_name_sep.strip('.!"№%:,.;(){}[]/><#@$^&*_+-=|')
     name_sep = name_sep.strip('.!"№%:,.;(){}[]/><#@$^&*_+-=|')
     #формируем имя файла день_месяц_год
-    if data[0] == 'Сьогодні':
+    if client_registry_data[0] == 'Сьогодні':
         file_name = f'{int(t.strftime("%d", t.localtime()))}_{t.strftime("%m", t.localtime())}_{t.strftime("%Y", t.localtime())}.json'
-    elif data[0] == 'Завтра':
+    elif client_registry_data[0] == 'Завтра':
         file_name = f'{int(t.strftime("%d", t.localtime())) + 1}_{t.strftime("%m", t.localtime())}_{t.strftime("%Y", t.localtime())}.json'
 
     #проверка имени и фамилии на отсутствие цифр и служебных символов
     if second_name_sep.isalpha() and name_sep.isalpha():
-        gym_registry = file_dir + file_name
+        gym_registry = registry_file_dir + file_name
         if not os.path.exists(gym_registry):  #создать файл для записи, если его нет
             rec = json.dumps({})
             with open(gym_registry, 'w') as gym_reg:
@@ -66,7 +70,7 @@ def check_registry(name, time_):
         else: #если файл с запрашиваемой датой есть, проверить в нём наличие записи с аналогичным именем
             with open(gym_registry, 'r') as gym_reg:
                 record = json.load(gym_reg)
-                user_id = str(data[2])
+                user_id = str(client_registry_data[2])
                 spy(user_id)
                 if user_id in record:
                     if record[user_id][0] >= REG_VALUE:
@@ -78,30 +82,29 @@ def check_registry(name, time_):
 def registry(name, time_, gym_registry):
     """
     Записать имя фамилию и время в файл, сформировать и вывести ответ.
-    На входе: name - строка с именем и фамилией; time_ - НН час, на котроый записать; gym_registry - путь к файлу.
+    На входе: name - строка с именем и фамилией; time_ - НН час, на который записать; gym_registry - путь к файлу.
     Возвращает строку с ответом при успешной записи
     """
     day = date.today()
-    if data[0] == 'Сьогодні':
-        str_day = str(day)
-        day_re = f'{str_day[8:10]} {month[str_day[5:7]]}'
-    elif data[0] == 'Завтра':
+    if client_registry_data[0] == 'Завтра':
         day = day + timedelta(days=1)
-        str_day = str(day)
-        day_re = f'{str_day[8:10]} {month[str_day[5:7]]}'
+
+    str_day = str(day)
+    day_to_registry = f'{str_day[8:10]} {month[str_day[5:7]]}'
+
     with open(gym_registry, 'r') as gym_reg_1:
         record = json.load(gym_reg_1)
-        user_id = str(data[2])
+        user_id = str(client_registry_data[2])
         if user_id in record:
             record[user_id][0] += 1
             record[user_id][1][name] = time_
         else:
-            record[user_id] = [1, {name : time_}]
+            record[user_id] = [1, {name: time_}]
     record_json = json.dumps(record)
     with open(gym_registry, 'w') as gym_reg:
         gym_reg.write(record_json)
 
-    return f'Дякуємо! Чекаємо на Вас {data[0].lower()}, {day_re} o {data[1]}:00'
+    return f'Дякуємо! Чекаємо на Вас {client_registry_data[0].lower()}, {day_to_registry} o {client_registry_data[1]}:00'
 
 def hour_range(day, hour=None):
     """
@@ -112,7 +115,7 @@ def hour_range(day, hour=None):
     Кортеж (от"НН", до"НН") с часами, на которые доступна запись в зависимости от дня недели и времени когда делается запрос
     No - значит запись на сегодня уже недоступна(при запросе в последний час работы зала или после закрытия)
     """
-    days = {0:(9, 21), 6:(8, 21), 1:(7, 23)}
+    days = {0:(9, 21), 6:(8, 21), 1:(7, 23)} # "код дня недели:(режим работы (ч) от, до)"
     if day not in days.keys():
         day = 1
     if hour == None or hour < days[day][0]:
@@ -139,7 +142,7 @@ def admin_mode(message):
     Формирует список с названиями файлов (один файл - один день со всеми записями внутри)
     Формирует экранные кнопки для выбора нужного файла
     """
-    files_list = os.listdir(file_dir)
+    files_list = os.listdir(registry_file_dir)
     files = [i for i in files_list]
     btn = types.InlineKeyboardMarkup()
     for file in sorted(files):
@@ -148,13 +151,16 @@ def admin_mode(message):
     bot.send_message(message.chat.id, reply_markup=btn, text='Список имеющихся файлов:')
 
 @bot.message_handler(commands=['spy'])
+"""
+Отвеча
+"""
 def spy_mode(message):
     with open(spy_file, 'r') as spy:
         dataset = json.load(spy)
-        users = len(dataset)
-        cycles = 0
-        for i in dataset.keys():
-            cycles += dataset[i]
+        users = len(dataset) #кол-во уникальных пользователей
+        cycles = 0 #общее кол-во запросов от всех пользователей
+        for cycle in dataset.keys():
+            cycles += dataset[cycle] #приба
         bot.send_message(message.chat.id, f'Users = {users}\nCycles = {cycles}')
 
 @bot.message_handler(commands=['alarm'])
@@ -162,8 +168,8 @@ def alarm(message):
     bot.send_message(message.chat.id, 'Генерирую файл с generated.txt c именами клиентов...')
     gym_registry_bot_alarm.alarm() #Генерирует json файл  generated.json с рандомными именами и временем
     user_dict = {}
-    file_name_txt = file_dir + 'generated.txt'
-    with open(file_dir + 'generated.json', 'r') as text:
+    file_name_txt = registry_file_dir + 'generated.txt'
+    with open(registry_file_dir + 'generated.json', 'r') as text:
         l = json.load(text)
         for user_id in l:
             for user_name in l[user_id][1]:
@@ -185,7 +191,7 @@ def alarm(message):
     txt = open(file_name_txt, 'rb')
     bot.send_document(message.chat.id, txt)
     os.remove(file_name_txt)
-    os.remove(file_dir + 'generated.json')
+    os.remove(registry_file_dir + 'generated.json')
 
 @bot.message_handler(content_types=['text'])
 def hours_buttons(message):
@@ -193,12 +199,12 @@ def hours_buttons(message):
     hour_now = int(t.strftime("%H", t.localtime()))
     # Обработка если клиент передал день
     if message.text == 'Сьогодні':
-        data.clear()
-        data.append(message.text)
+        client_registry_data.clear()
+        client_registry_data.append(message.text)
         range_ = hour_range(day_now, hour_now)
         if range_ == 'No':
             bot.send_message(message.chat.id,"Нажаль на сьогодні вже запис неможливий, спробуйте записатись на завтра.")
-            data.clear()
+            client_registry_data.clear()
         else:
             hours = [i for i in range(range_[0], range_[1])]
             btn = types.InlineKeyboardMarkup()
@@ -207,8 +213,8 @@ def hours_buttons(message):
                 btn.add(btn_hour)
             bot.send_message(message.chat.id, reply_markup=btn,text='Оберіть приблизно годину, коли ви плануете відвідати зал:')
     elif message.text == 'Завтра':
-        data.clear()
-        data.append(message.text)
+        client_registry_data.clear()
+        client_registry_data.append(message.text)
         tomorrow = day_now + 1
         if tomorrow == 7:
             range_ = hour_range(0)
@@ -216,7 +222,7 @@ def hours_buttons(message):
             range_ = hour_range(tomorrow)
         if range_ == 'No':
             bot.send_message(message.chat.id,"Нажаль на сьогодні вже запис неможливий, спробуйте записатись на завтра.")
-            data.clear()
+            client_registry_data.clear()
         else:
             hours = [i for i in range(range_[0], range_[1])]
             btn = types.InlineKeyboardMarkup()
@@ -229,10 +235,10 @@ def hours_buttons(message):
         name = message.text.strip()
         if ' ' not in name:
             bot.send_message(message.chat.id, "Не вірно введене ім'я. Введіть своє справжнє ім'я в форматі: Прізвище Ім'я . Наприклад: Черкашина Олеся")
-        elif not data:
+        elif not client_registry_data:
             bot.send_message(message.chat.id, "Ви хочете записатись на сьогодні чи на завтра? Будь-ласка оберіть варіант знизу:")
         else: #пустить процедуру проверки имени с последующей записей
-            bot.send_message(message.chat.id, check_registry(message.text, data[1]))
+            bot.send_message(message.chat.id, check_registry(message.text, client_registry_data[1]))
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
@@ -242,13 +248,13 @@ def callback_inline(call):
     if len(call.data) in (1,2) and str(call.data).isdigit(): # обработка если клиент передал время
         hour = call.data
         user_id = call.from_user.id
-        data.append(hour)
-        data.append(user_id)
+        client_registry_data.append(hour)
+        client_registry_data.append(user_id)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Вкажіть своє справжнє Прізвище та Ім'я. Наприклад: Черкашина Олеся")
     elif '_' in str(call.data) and str(call.data)[10:] == '.json': # после выбора файла открыть его и выслать
         user_dict = {}
-        file_name_txt = file_dir + str(call.data)[:10] + '.txt'
-        with open(file_dir + str(call.data), 'r') as text:
+        file_name_txt = registry_file_dir + str(call.data)[:10] + '.txt'
+        with open(registry_file_dir + str(call.data), 'r') as text:
             l = json.load(text)
             for user_id in l:
                 for user_name in l[user_id][1]:
